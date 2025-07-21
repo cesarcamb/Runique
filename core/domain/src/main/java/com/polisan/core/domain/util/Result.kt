61d1c571c280ba83d3857a1e7bed40eb53664c52
cesarcamb <cesarmendozabenavides@gmail.com>
2025-07-21 16:08:33 -0600

    String resource management

--- core/domain/src/main/java/com/polisan/core/domain/util/Result.kt
+++ core/domain/src/main/java/com/polisan/core/domain/util/Result.kt
@@ -5,26 +5,15 @@ sealed interface Result<out D, out E: Error> {
     data class Error<out E: com.polisan.core.domain.util.Error>(val error: E): Result<Nothing, E>
 }
 
-enum class PasswordValidationError: Error {
-    TOO_SHORT,
-    MISSING_ONE_DIGIT
-}
-
-fun validatePassword(password: String): Result<Unit, PasswordValidationError> {
-    // logic
-    return Result.Error(PasswordValidationError.MISSING_ONE_DIGIT)
-}
-
-fun handleError(result: Result<Unit, PasswordValidationError>) {
-    when(result) {
-        is Result.Success ->  {
-            // Valid password
-        }
-        is Result.Error -> {
-            when(result.error) {
-                PasswordValidationError.TOO_SHORT -> {TODO()}
-                PasswordValidationError.MISSING_ONE_DIGIT -> {TODO()}
-            }
-        }
+inline fun <T, E: Error, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
+    return when(this) {
+        is Result.Error -> Result.Error(error)
+        is Result.Success -> Result.Success(map(data))
     }
 }
+
+fun <T, E: Error> Result<T, E>.asEmptyDataResult(): EmptyDataResult<E> {
+    return map { }
+}
+
+typealias EmptyDataResult<E> = Result<Unit, E>
