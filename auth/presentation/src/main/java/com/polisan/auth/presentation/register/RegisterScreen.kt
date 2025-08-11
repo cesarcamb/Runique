c7a338635f6caa1ba9ad94399519cf0e70004710
cesarcamb <cesarmendozabenavides@gmail.com>
2025-08-11 11:36:50 -0600

    Register Screen UI

---
+++ auth/presentation/src/main/java/com/polisan/auth/presentation/register/RegisterScreen.kt
@@ -0,0 +1,226 @@
+package com.polisan.auth.presentation.register
+
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.withLink
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.polisan.auth.domain.PasswordValidationState
+import com.polisan.auth.domain.UserDataValidator
+import com.polisan.core.presentation.designsystem.CheckIcon
+import com.polisan.core.presentation.designsystem.CrossIcon
+import com.polisan.core.presentation.designsystem.EmailIcon
+import com.polisan.core.presentation.designsystem.Poppins
+import com.polisan.core.presentation.designsystem.R
+import com.polisan.core.presentation.designsystem.RuniqueDarkRed
+import com.polisan.core.presentation.designsystem.RuniqueGray
+import com.polisan.core.presentation.designsystem.RuniqueGreen
+import com.polisan.core.presentation.designsystem.RuniqueTheme
+import com.polisan.core.presentation.designsystem.components.GradientBackground
+import com.polisan.core.presentation.designsystem.components.RuniqueActionButton
+import com.polisan.core.presentation.designsystem.components.RuniquePasswordTextField
+import com.polisan.core.presentation.designsystem.components.RuniqueTextField
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun RegisterScreenRoot(
+    onSignInClick: () -> Unit,
+    onSuccessfulRegistration: () -> Unit,
+    viewModel: RegisterViewModel = koinViewModel()
+) {
+    RegisterScreen(
+        state = viewModel.state,
+        onAction = viewModel::onAction
+    )
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+private fun RegisterScreen(
+    state: RegisterState,
+    onAction: (RegisterAction) -> Unit
+) {
+    GradientBackground {
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .padding(horizontal = 16.dp)
+                .padding(vertical = 32.dp)
+                .padding(top = 16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.create_account),
+                style = MaterialTheme.typography.headlineMedium
+            )
+            val annotatedString = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        fontFamily = Poppins,
+                        color = RuniqueGray
+                    )
+                ) {
+                    append(stringResource(R.string.already_have_an_account) + " ")
+                    withLink(
+                        LinkAnnotation.Clickable(
+                            tag = "clickable_text",
+                            styles = TextLinkStyles(
+                                style = SpanStyle(
+                                    fontWeight = FontWeight.SemiBold,
+                                    color = MaterialTheme.colorScheme.primary,
+                                    fontFamily = Poppins
+                                )
+                            ),
+                            linkInteractionListener = {
+                                onAction(RegisterAction.OnLoginClick)
+                            }
+                        )
+                    ) {
+                        append(stringResource(id = R.string.login))
+                    }
+                }
+            }
+
+            Text(
+                text = annotatedString,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(48.dp))
+            RuniqueTextField(
+                state = state.email,
+                startIcon = EmailIcon,
+                endIcon = if(state.isEmailValid) {
+                    CheckIcon
+                } else null,
+                hint = stringResource(R.string.example_email),
+                title = stringResource(R.string.email),
+                modifier = Modifier.fillMaxWidth(),
+                additionalInfo = stringResource(R.string.must_be_a_valid_email),
+                keyboardType = KeyboardType.Email
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            RuniquePasswordTextField(
+                state = state.password,
+                isPasswordVisible = state.isPasswordVisible,
+                onTogglePasswordVisibility = {
+                    onAction(RegisterAction.OnTogglePasswordVisibilityClick)
+                },
+                hint = stringResource(R.string.password),
+                title = stringResource(R.string.password),
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    R.string.at_least_x_characters,
+                    UserDataValidator.MIN_PASSWORD_LENGTH
+                ),
+                isValid = state.passwordValidationState.hasMinLength
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    R.string.at_least_one_number,
+                ),
+                isValid = state.passwordValidationState.hasNumber
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    R.string.contains_lowercase_character,
+                ),
+                isValid = state.passwordValidationState.hasMinLength
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            PasswordRequirement(
+                text = stringResource(
+                    R.string.contains_uppercase_char,
+                    UserDataValidator.MIN_PASSWORD_LENGTH
+                ),
+                isValid = state.passwordValidationState.hasMinLength
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            RuniqueActionButton(
+                text = stringResource(R.string.register),
+                isLoading = state.isRegistering,
+                enabled = state.canRegister,
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    onAction(RegisterAction.OnRegisterClick)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun PasswordRequirement(
+    text: String,
+    isValid: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Icon(
+            imageVector = if(isValid) {
+                CheckIcon
+            } else {
+                CrossIcon
+            },
+            contentDescription = null,
+            tint = if(isValid) RuniqueGreen else RuniqueDarkRed
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            fontSize = 14.sp
+        )
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Preview
+@Composable
+private fun RegisterScreenPreview() {
+    RuniqueTheme(){
+       RegisterScreen(
+           state = RegisterState(
+               passwordValidationState = PasswordValidationState(
+                   hasNumber = true,
+                   hasLowerCaseCharacter = true,
+                   hasUpperCaseCharacter = true,
+                   hasMinLength = true,
+               )
+           ),
+           onAction = {}
+       )
+   }
+}
